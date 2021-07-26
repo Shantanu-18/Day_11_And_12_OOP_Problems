07350e25f6f6d8b9337c1380c5e9ccc50a5d7e72
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-26 22:00:20 +0530

    "[Shantanu] Added : JSON Inventory Data Management"

---
+++ Day_11_And_12_OOP_Problems/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Day_11_And_12_OOP_Problems
+{
+    public class FileReader
+    {
+        public static List<Inventory> inventories = new List<Inventory>();
+
+        public List<Inventory> GetInventry()
+        {
+            string text = File.ReadAllText("C:\\Users\\shant\\Documents\\Data.json");
+            inventories = JsonConvert.DeserializeObject<List<Inventory>>(text);
+            return inventories;
+        }
+
+    }
+}
---
+++ Day_11_And_12_OOP_Problems/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_11_And_12_OOP_Problems
+{
+    public class Inventory
+    {
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Price { get; set; }
+    }
+}
