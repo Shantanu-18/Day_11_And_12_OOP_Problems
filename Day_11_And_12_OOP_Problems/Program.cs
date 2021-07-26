32dbc08695681d6a2211ce588dd9ed8e04b650e0
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-26 22:00:20 +0530

    "[Shantanu] Added : JSON Inventory Data Management"

---
+++ Day_11_And_12_OOP_Problems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day_11_And_12_OOP_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileReader fileReader = new FileReader();
+            fileReader.GetInventry();
+        }
+    }
+}
